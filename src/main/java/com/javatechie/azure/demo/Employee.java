c24d0b198a1dd1d4b206e206c29cde0ffa2c2497
mohan <mohan@DESKTOP-7FGEPFC>
2022-04-24 08:59:43 +0530

    test

---
+++ src/main/java/com/javatechie/azure/demo/Employee.java
@@ -0,0 +1,44 @@
+package com.javatechie.azure.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String dept;
+    private long salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+    
+    
+}
