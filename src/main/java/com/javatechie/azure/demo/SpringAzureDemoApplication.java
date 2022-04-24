32f76a0bd984cb5dbdcd1c14f1151d86179b9384
mohan <mohan@DESKTOP-7FGEPFC>
2022-04-24 08:53:34 +0530

    commit code

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -1,8 +1,13 @@
 package com.javatechie.azure.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,5 +22,24 @@ public class SpringAzureDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureDemoApplication.class, args);
 	}
+	
+	 @Autowired
+	    private EmployeeRepository repository;
+
+	    @PostMapping("/employee")
+	    public Employee addEmployee(@RequestBody Employee1 employee) {
+	    	Employee emp = new Employee();
+	    	emp.setDept(employee.getDept());
+	    	emp.setName(employee.getName());
+	    	emp.setSalary(employee.getSalary());
+	        return repository.save(emp);
+	    }
+
+	    @GetMapping("/employees")
+	    public List<Employee> getEmployees() {
+	        return repository.findAll();
+	    }
+
+
 
 }
